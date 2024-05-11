0637a174032fde6061cb3da3bfa36115fa4f0021
mgravemeyer <m.gravemeyer@icloud.com>
2024-05-11 13:58:50 +0200

    added impressum and datenschutz pages

---
+++ src/routes/impressum/+page.svelte
@@ -0,0 +1,55 @@
+<div class="p-8 sm:p-24">
+	<a class="hover:cursor-pointer" href="/">
+		Zurück
+	</a>
+
+	<h1 class="mt-8">Impressum</h1>
+
+	<h2>Angaben gem&auml;&szlig; &sect; 5 TMG</h2>
+	<p>
+		Maximilian Gravemeyer
+		<br />
+		Softwareentwicklung als Dienstleistung, Ver&ouml;ffentlichung von
+		Artikeln.
+		<br />
+		Kopenhagener Straße 27
+		<br />
+		10437 Berlin
+	</p>
+
+	<h2>Kontakt</h2>
+	<p>
+		Telefon: +49 175 7441765
+		<br />
+		E-Mail: maximilian@gravemeyer.com
+	</p>
+
+	<h2>Redaktionell verantwortlich</h2>
+	<p>Maximilian Gravemeyer, Kopenhagener Straße 27, 10437 Berlin</p>
+
+	<h2>EU-Streitschlichtung</h2>
+	<p>
+		Die Europ&auml;ische Kommission stellt eine Plattform zur
+		Online-Streitbeilegung (OS) bereit:{' '}
+		<a
+			href="https://ec.europa.eu/consumers/odr/"
+			target="_blank"
+			rel="noopener noreferrer"
+		>
+			https://ec.europa.eu/consumers/odr/
+		</a>
+		.<br /> Unsere E-Mail-Adresse finden Sie oben im Impressum.
+	</p>
+
+	<h2>
+		Verbraucher&shy;streit&shy;beilegung/Universal&shy;schlichtungs&shy;stelle
+	</h2>
+	<p>
+		Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren
+		vor einer Verbraucherschlichtungsstelle teilzunehmen.
+	</p>
+
+	<p>
+		Quelle: <a href="https://www.e-recht24.de">e-recht24.de</a>
+	</p>
+</div>
