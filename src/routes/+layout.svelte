c67081f67ee4a16379e6af1123cc27b9d609b7cf
mgravemeyer <m.gravemeyer@icloud.com>
2024-05-11 14:27:20 +0200

    updated meta tags

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,8 +5,11 @@
 </script>
 
 <svelte:head>
-	<meta name="description" content="Freelancer specialized in the development of complex web solutions | 📍Berlin | ✅15+ Projects | React/Next.js, Svelte/SvelteKit, TypeScript, Shopware 6, PHP (Symfony, Laravel).">
+	<meta name="description" content="Freelancer specialized in the development of complex web solutions | 📍Berlin | ✅15+ Projects | React/Next.js, VueJS/Nuxt, TypeScript, Shopware 6, PHP (Symfony, Laravel).">
+	<meta name="description" lang="de" content="Freelancer spezialisiert auf die Entwicklung komplexer Webanwendungen | 📍Berlin | ✅15+ Projekte | React/Next.js, VueJS/Nuxt, TypeScript, Shopware 6, PHP (Symfony, Laravel).">
 	<meta name="keywords" content="Gravemeyer, Maximilian Gravemeyer, Entwickler, Freelancer">
+	<meta name="author" content="Maximilian Gravemeyer">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </svelte:head>
 
 <ParaglideJS {i18n}>
